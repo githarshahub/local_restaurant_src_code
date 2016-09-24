92382bf7ce47842eeeb49a35104844560b6d7e1c
githarshahub <sendamail2harsh@gmail.com>
2016-09-23 22:27:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -31,11 +31,10 @@ app.get('/webhook/', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
-	console.log("Entered the post method");
     for (var i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            sendMessage(event.sender.id, {text: "This is just the beginning. Watch out this space"});
         }
     }
     res.sendStatus(200);
